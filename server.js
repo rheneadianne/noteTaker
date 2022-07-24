725600b6a5bac3d18420d9412a29d0ffb4905cbb
rhenea <rheneadianne@gmail.com>
2022-07-24 10:07:47 -0400

    notes populating to left sidebar

--- server.js
+++ server.js
@@ -14,12 +14,27 @@ app.use(express.json())
 id = () => uuidv4() //testing uuid
 console.log(id())
 
+// 
+// WHEN I click on the link to the notes page
+// THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column --> probably needs db.json
+// WHEN I enter a new note title and the note’s text
+// THEN a Save icon appears in the navigation at the top of the page ---> it already does that wym
+// WHEN I click on the Save icon
+// THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
+// WHEN I click on an existing note in the list in the left-hand column
+// THEN that note appears in the right-hand column
+// WHEN I click on the Write icon in the navigation at the top of the page
+// THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
 
-app.get("/", (req, res) => { // gets index.html for landing page
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./db/db.json"));
+})
+
+app.get("/", (req, res) => { // gets index.html for landing page THEN I am presented with a landing page with a link to a notes page
     res.sendFile(path.join(__dirname, "./public/index.html)"));
 });
 
-app.get("/notes", (req, res) => { // gets notes.html for /notes path
+app.get("/notes", (req, res) => { // gets notes.html for /notes path 
     res.sendFile(path.join(__dirname, "./public/notes.html"));
   });
 
